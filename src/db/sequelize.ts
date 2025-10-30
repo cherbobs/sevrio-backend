adc494530b4775907c28d1a1904ebad5919d9702
cherbobs <eva.cherbonnier974@gmail.com>
2025-10-31 00:14:24 +0100

    temp log

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -2,6 +2,14 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
 export const sequelize = new Sequelize({
   dialect: "postgres",
   host: process.env.DB_HOST,
@@ -9,15 +17,10 @@ export const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  logging: false,
-  dialectOptions: {
-    ssl: { require: true, rejectUnauthorized: false },
-    family: 4,
-    keepAlive: true,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
+  logging: console.log,
+  //   dialectOptions: {
+  //     ssl: { require: true, rejectUnauthorized: false },
+  //     family: 4,
+  //     keepAlive: true,
+  //   },
 });
